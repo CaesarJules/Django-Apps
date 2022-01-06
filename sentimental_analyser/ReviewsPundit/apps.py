82d4c9decd45b7ec62c92a7d4380c5f02bc02057
CaesarJules <42262367+CaesarJules@users.noreply.github.com>
2022-01-06 03:33:11 -0500

    ReviewsPundit Django App

--- sentimental_analyser/ReviewsPundit/apps.py
+++ sentimental_analyser/ReviewsPundit/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.conf import settings
+import os
+import pickle
 
 
 class ReviewspunditConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'ReviewsPundit'
+
+    path = os.path.join(settings.MODELS, 'model.p')
+
+    with open(path, 'rb') as p:
+        data = pickle.load(p)
+    model = data['model']
+    vectorizer = data['vectorizer']
+
--- sentimental_analyser/ReviewsPundit/views.py
+++ sentimental_analyser/ReviewsPundit/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .apps import ReviewspunditConfig
+from django.http import JsonResponse
+from rest_framework.views import APIView
 
-# Create your views here.
+class call_model(APIView):
+    def get(self, request):
+        if request.method == 'GET':
+            #Retrieve the input sentence
+            text = request.GET.get('text')
+            #Vectorize the document
+            vector = ReviewspunditConfig.vectorizer.transform([text])
+            prediction = ReviewspunditConfig.model.predict(vector)[0]
+
+            return JsonResponse({'text_sentiment': prediction})
